dbae69d9076e80290cfb7dfa6d10a15f07532661
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-08-13 00:54:16 +0700

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 // 	PRIMARY KEY (`id`)
 //   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-//B1 tạo struct là 1 model của TodoItems
+//B2 tạo struct là 1 model của TodoItems
 type TodoItem struct {
 	//'json:"id" gorm:"column:id;` => gọi là tag: là những tuỳ chọn sử dụng để khai báo trường
 	//json:
@@ -27,12 +27,18 @@ type TodoItem struct {
 	Status string `json:"status" gorm:"column:status;"`
 }
 
-//method TableName trả về 1 string là tên của bảng
+type TodoItemUpdate struct {
+	//khi update ta để con trỏ string. lúc update bên gorm sẽ kiểm tra xem con trỏ đó có giá trị hay không nếu có giá trị thì lúc đó mới update
+	Title *string `json:"title" gorm:"column:title;"`
+}
+
+//B3: method TableName trả về 1 string là tên của bảng
 func (TodoItem) TableName() string {
 	return "todo_items"
 }
 
 func main() {
+	//B1 connect db
 	mysqlConnStr, ok := os.LookupEnv("MYSQL_CONNECTION")
 
 	if !ok {
@@ -45,8 +51,35 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	newTodoItem := TodoItem{Title: "English", Status: "Doing"}
-	db.Create(&newTodoItem)
-	fmt.Println(newTodoItem)
-	//insert new todo item
+
+	//B4.1 insert new todo item
+	// newTodoItem := TodoItem{Title: "English", Status: "Doing"}
+	// if err := db.Create(&newTodoItem); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	//B4.2 select all with where
+	var todos []TodoItem
+	db.Where("status = ?", "Doing").Find(&todos)
+	fmt.Println(todos)
+
+	//B4.3 select one
+	var todo TodoItem
+	if err := db.Where("id = 1").First(&todo); err != nil {
+		//Khác nhau giữa log và fmt là log mình có thể log thêm thời gian
+		log.Println(err)
+	}
+	fmt.Println(todo)
+
+	//B5 delete
+	//db.Table(TodoItem{}.TableName()).Where("id=3").Delete(nil)
+
+	//B6.1 Update c1
+	// db.Table(TodoItem{}.TableName()).Where("id=3").Updates(map[string]interface{}{
+	// 	"title": "lean go",
+	// })
+
+	//B6.1 Update c2
+	newTitle := "lean go"
+	db.Table(TodoItem{}.TableName()).Where("id=3").Updates(&TodoItemUpdate{Title: &newTitle})
 }
